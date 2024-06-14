54676d7e65c0a733ce294d11ec75ce65336d7fbe
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-14 20:38:00 +0100

    Added account model

---
+++ src/models/Account.js
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+
+const todoSchema = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+});
+
+const Todo = model("Todo", todoSchema);
+
+export default Todo;
