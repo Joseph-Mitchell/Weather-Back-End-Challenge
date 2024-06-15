9ce424e441cc2abac62c6f7a7950cdc313497e4b
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-15 15:12:28 +0100

    Account.middleware.js: Unit Test 2 Complete

--- test/middleware/Account.middleware.js
+++ test/middleware/Account.middleware.js
@@ -1,13 +1,27 @@
 import { describe, it } from "mocha";
 import AccountValidator from "../../src/middleware/Account.middleware.js";
 import { assert } from "chai";
+import sinon from "sinon";
 
 describe("validateRegDetails", () => {
-    it("should return an array in normal circumstances", () => {
+    it("should return an array in normal conditions", () => {
         //Act
         const actual = AccountValidator.validateRegDetails();
         
         //Assert
         assert.isArray(actual);
-    })
-})
+    });
+});
+
+describe("handleValidationErrors", () => {
+    it("should call next in normal conditions", () => {
+        //Arrange
+        const nextStub = sinon.stub();
+        
+        //Act
+        AccountValidator.handleValidationErrors({}, {}, nextStub);
+        
+        //Assert
+        sinon.assert.called(nextStub);
+    });
+});
