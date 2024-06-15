101a38c518836b7bfb65d3cff5fd0f493ebbf376
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-15 19:12:26 +0100

    Register Account: Integration Test 6 Complete

--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -94,5 +94,13 @@ describe("Integration Tests", () => {
             //Assert
             assert.equal(actual.status, 400);
         });
+        
+        it("should respond 400 with missing password", async () => {
+            //Act
+            const actual = await requester.post("/register").send(testData.newAccounts.missingPassword);
+            
+            //Assert
+            assert.equal(actual.status, 400);
+        });
     });
 })
