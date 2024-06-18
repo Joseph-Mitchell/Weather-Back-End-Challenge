2cd1f7a144ff6f821bbd9d86a8e41882d4de7398
Joseph Mitchell <joejmitchell@hotmail.co.uk>
2024-06-18 21:48:03 +0100

    Account.middleware.js: Unit Test 5 Complete

--- test/middleware/Account.middleware.test.js
+++ test/middleware/Account.middleware.test.js
@@ -4,51 +4,70 @@ import { assert } from "chai";
 import sinon from "sinon";
 import jwt from "jsonwebtoken";
 
-describe("validateRegDetails", () => {
-    it("should return an array in normal conditions", () => {
-        //Act
-        const actual = AccountValidator.validateRegDetails();
-        
-        //Assert
-        assert.isArray(actual);
-    });
-});
 
-describe("handleValidationErrors", () => {
-    it("should call next in normal conditions", () => {
-        //Arrange
-        const nextStub = sinon.stub();
-        
-        //Act
-        AccountValidator.handleValidationErrors({}, {}, nextStub);
-        
-        //Assert
-        sinon.assert.called(nextStub);
+describe("Middleware: ", () => {
+    describe("validateRegDetails", () => {
+        it("should return an array in normal conditions", () => {
+            //Act
+            const actual = AccountValidator.validateRegDetails();
+            
+            //Assert
+            assert.isArray(actual);
+        });
     });
-});
 
-describe("authenticateToken", () => {   
-    it("should call next in normal conditions", () => {
-        //Arrange
-        sinon.stub(jwt, "verify").callsArgWith(2, 0, { token: 1 });
-        const nextStub = sinon.stub();
-        const testReq = {
-            headers: {
-                "x-access-token": 1
-            }
-        };
-        const stubbedRes = {
-            status: sinon.stub().returnsThis,
-            send: sinon.stub()
-        }
+    describe("handleValidationErrors", () => {
+        it("should call next in normal conditions", () => {
+            //Arrange
+            const nextStub = sinon.stub();
+            
+            //Act
+            AccountValidator.handleValidationErrors({}, {}, nextStub);
+            
+            //Assert
+            sinon.assert.called(nextStub);
+        });
+    });
+
+    describe("authenticateToken", () => {
+        it("should call next in normal conditions", () => {
+            //Arrange
+            const nextStub = sinon.stub();
+            const testReq = {
+                headers: {
+                    "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzE4NzQzMzE1fQ.YAk7bBQyFOvVtD0tsDV8zU7MVRxyJMCvmFvbqPkXZhQ"
+                }
+            };
+            const stubbedRes = {
+                status: sinon.stub().returnsThis(),
+                send: sinon.stub()
+            };
+            
+            //Act
+            AccountValidator.authenticateToken(testReq, stubbedRes, nextStub);
+            
+            //Assert
+            sinon.assert.called(nextStub);
+        });
         
-        //Act
-        AccountValidator.authenticateToken(testReq, stubbedRes, nextStub);
-        
-        //Assert
-        sinon.assert.called(nextStub);
-        
-        //Cleanup
-        jwt.verify.restore();
-    })
+        it("should respond with 401 if no token provided in header", () => {
+            console.log(jwt.sign({ id: 1 }, process.env.SECRET));
+            
+            //Arrange
+            const nextStub = sinon.stub();
+            const testReq = {
+                headers: {}
+            };
+            const stubbedRes = {
+                status: sinon.stub().returnsThis(),
+                send: sinon.stub()
+            };
+            
+            //Act
+            AccountValidator.authenticateToken(testReq, stubbedRes, nextStub);
+            
+            //Assert
+            sinon.assert.calledWith(stubbedRes.status, 401);
+        });
+    });
 });
